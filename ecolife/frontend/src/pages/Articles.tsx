963ee0e39d5ee6e433894880ed4c8c307116dfd9
AndonijaT <andonija.tdrv@gmail.com>
2024-06-12 03:48:36 +0200

    fixes

--- ecolife/frontend/src/pages/Articles.tsx
+++ ecolife/frontend/src/pages/Articles.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import './Articles.css';
+import './../styles/Articles.css';
 import VideoBackground from '../sections/VideoBackground';
 import { collection, getDocs } from 'firebase/firestore';
 import { db } from '../firebaseConfig';
